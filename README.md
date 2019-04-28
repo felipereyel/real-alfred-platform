8dd8be96222b339b88ea110c90006e821fbf4758
{ <felipe.reyel@gmail.com>
2019-04-27 21:44:58 -0300

    backend Done! fuckyez

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tor_aux
 
+Esse projeto cria uma ferramenta que auxilia na busca e download de suas series favoritas
--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const axios = require('axios');
 const fs = require('fs');
 
-let URLs = [];
+function copy(aObject) {
+    if (!aObject) {
+      return aObject;
+    }
+  
+    let v;
+    let bObject = Array.isArray(aObject) ? [] : {};
+    for (const k in aObject) {
+      v = aObject[k];
+      bObject[k] = (typeof v === "object") ? copy(v) : v;
+    }
+  
+    return bObject;
+};
 
-async function filterQualitySource(names, quality, source){
+async function filterQualitySource(names, quality, source){//ta TOPZERA
     //filtro de Qualidade
     let namesFilteredByQuality = names.filter((name) => {
         if(name.search(quality) != -1){
@@ -30,12 +43,12 @@ async function filterQualitySource(names, quality, source){
     return namesFilteredByQualityBySource[0];
 };
 
-async function createURL(fileName){
+async function createURL(fileName){//ta PIka daS GalaxiA
     let URL = 'https://linx.cloud/' + fileName.replace('.HEVC-PSA', '').replace('.','-').toLowerCase();
     return URL;
 };
 
-async function getEpNamesFromURL(name, URL){
+async function getEpNamesFromURL(name, URL){//melhor que essa so duas essa
     return await axios.get(URL).then(response => {
         let ss = response.data.split('\n');
         let names = ss.filter((line) => {
@@ -49,15 +62,15 @@ async function getEpNamesFromURL(name, URL){
     });
 };
 
-async function getJSONDataBase(fileName){
+async function getJSONDataBase(fileName){//double triple piroca
     return JSON.parse(fs.readFileSync(fileName));
 };
 
-async function pushJSONDataBase(fileName, jsonfile){
+async function pushJSONDataBase(fileName, jsonfile){//nao testada
     fs.writeFileSync(fileName, JSON.stringify(jsonfile, null, 2));
 };
 
-async function filterNamesToDownload(names, notDowloaded){
+async function filterNamesToDownload(names, notDowloaded){//le TOPERson
     let namesToDownload = {};
 
     notDowloaded.map((epNotDownloaded) => {
@@ -72,30 +85,49 @@ async function filterNamesToDownload(names, notDowloaded){
 };
 
 async function findEps(serieInfo){
-    names = await getEpNamesFromURL(serieInfo.name, serieInfo.URL);
-    namesToDownload = await filterNamesToDownload(names, serieInfo.notDownloaded);
+
+    let serieInfoCopy = copy(serieInfo);
+    let URLs = [];
+
+    let names = await getEpNamesFromURL(serieInfoCopy.name, serieInfoCopy.URL);
+    let namesToDownload = await filterNamesToDownload(names, serieInfoCopy.notDownloaded);
 
     for(var ep in namesToDownload){
-        if(namesToDownload[ep] == []){
+        if(!(Array.isArray(namesToDownload[ep]) && namesToDownload[ep].length)){
             continue;
         }
-        nameToDownloadFiltered = await filterQualitySource(namesToDownload[ep], serieInfo.quality, serieInfo.source);
-        serieInfo.notDownloaded = serieInfo.notDownloaded.filter((epNotDownloaded) => {
+        let nameToDownloadFiltered = await filterQualitySource(namesToDownload[ep], serieInfoCopy.quality, serieInfoCopy.source);
+        serieInfoCopy.notDownloaded = serieInfoCopy.notDownloaded.filter((epNotDownloaded) => {
             return epNotDownloaded != ep;
         });
         URLs.push(await createURL(nameToDownloadFiltered));
     }
 
-    return serieInfo;    
+    return {serieInfoCopy, URLs};    
 };
 
-async function run(){
+async function run(serieCode = 'all'){
     let seriesInfo = await getJSONDataBase('series.json');
-    for (let serie in seriesInfo){
-        seriesInfo[serie] = await findEps(seriesInfo[serie]);
+    let allURLs = [];
+
+    if(serieCode == 'all'){
+        for (let serie in seriesInfo){
+            const {serieInfoCopy, URLs} = await findEps(seriesInfo[serie]);
+            seriesInfo[serie] = serieInfoCopy;
+            //console.log(URLs);
+            allURLs.push({tittle: seriesInfo[serie].tittle, URLs});
+        }
     }
+    else{
+        const {serieInfoCopy, URLs} = await findEps(seriesInfo[serieCode]);
+        seriesInfo[serieCode] = serieInfoCopy;
+        //console.log(URLs);
+        allURLs.push({tittle: seriesInfo[serieCode].tittle, URLs});
+    }
+
     await pushJSONDataBase('series.json', seriesInfo);
-    console.log(URLs);
+    console.log(JSON.stringify(allURLs, null, 2));
+    return allURLs;
 }; 
 
-run();
+run('cloak');
