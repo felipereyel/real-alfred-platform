38e19e988b0d2aeb20c6a5f8a0e6f9356a7cd383
Felipe Reyel Feitosa de Sousa <felipe.reyel@gmail.com>
2021-01-25 19:34:43 +0000

    update cors

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
-app.use(cors({ origin: true }));
+app.use(cors());
+app.options('*', cors());
 
 const { SEASON_VALIDATOR, EPISODE_VALIDATOR } = require("./constants");
 const { group } = require("./processing");
